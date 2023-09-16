420f9d54fdefc1e6b08f935735fb89c030a5b78c
meerimabdykadyrova <141637626+meerimabdykadyrova@users.noreply.github.com>
2023-09-16 11:09:38 +0300

    Add files via upload

---
+++ project_program_for_students/project_program_for_students/js/lesson.js
@@ -0,0 +1,68 @@
+
+//PHONE VALIDATOR
+
+
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
+
+
+
+// phoneInput.
+
+const regExp = /^\+996 [5792]\d{2} \d{2}-\d{2}-\d{2}$/
+
+
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)){
+        phoneResult.innerHTML = "OK"
+        phoneResult.style.color = 'green'
+    }else {
+        phoneResult.innerHTML = 'NOT OK'
+        phoneResult.style.color = 'red'
+    }
+}
+
+// tab
+
+
+//tab slider
+const tabContendBlocks = document.querySelectorAll('.tab_content_block')
+const tabsParentBlock = document.querySelector('.tab_content_items')
+const tabsBlocks = document.querySelectorAll('.tab_content_item')
+
+
+
+const hideTabContend = () => {
+    tabContendBlocks.forEach(tabContendBlock=> {
+        tabContendBlock.style.display = 'none'
+    })
+    tabsBlocks.forEach(tabBlock => {
+        tabBlock.classList.remove('tab_content_item_active')
+
+    })
+}
+const showTabContends = (indexElement = 0) => {
+    tabContendBlocks[indexElement].style.display = 'block'
+    tabsBlocks[indexElement].classList.add('tab_content_item_active')
+}
+hideTabContend()
+showTabContends()
+
+tabsParentBlock.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')){
+        tabsBlocks.forEach((tabBlock, tabIndex)=> {
+            if (event.target === tabBlock){
+                hideTabContend()
+                showTabContends(tabIndex)
+            }
+        })
+    }
+}
+let sliderIndex = 0
+const autoSlider = () => {
+    hideTabContend()
+    sliderIndex = (sliderIndex + 1) % tabContendBlocks.length
+    showTabContends(sliderIndex)
+}
+setInterval(autoSlider, 3000)
